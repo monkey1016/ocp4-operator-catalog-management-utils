8a7512abf81a273b97237fc0b7fa1d4aaef0a806
Lev Shulman <lshulman@redhat.com>
2020-05-30 11:37:38 -0600

    reading of entries unit test

--- src/test/java/redhat/ocp4/operators/catalog/utils/OperatorCatalogUtilApplicationTests.java
+++ src/test/java/redhat/ocp4/operators/catalog/utils/OperatorCatalogUtilApplicationTests.java
@@ -1,13 +1,32 @@
 package redhat.ocp4.operators.catalog.utils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
 
 @SpringBootTest
 class OperatorCatalogUtilApplicationTests {
 
+	@Value("classpath:operatorhub-5-30-20.tar.gz")
+	Resource operatorHubArchive;
+	
 	@Test
 	void contextLoads() {
+		assertTrue(operatorHubArchive.exists());
+		try {
+			String[] archiveEntries = GtarUtil.listEntriesInGtarArchive(operatorHubArchive.getInputStream());
+			assertEquals(2948, archiveEntries.length);
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
 	}
 
 }
