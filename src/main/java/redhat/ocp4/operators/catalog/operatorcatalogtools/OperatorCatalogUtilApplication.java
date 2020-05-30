5cb0c3ebb47dd26f0c960d5c6f2e8a747ba9dc90
Lev Shulman <lshulman@redhat.com>
2020-05-30 10:14:37 -0600

    initial import of boilerplate project form spring initializr

---
+++ src/main/java/redhat/ocp4/operators/catalog/operatorcatalogtools/OperatorCatalogUtilApplication.java
@@ -0,0 +1,13 @@
+package redhat.ocp4.operators.catalog.operatorcatalogtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatorCatalogUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperatorCatalogUtilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/redhat/ocp4/operators/catalog/operatorcatalogtools/OperatorCatalogUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package redhat.ocp4.operators.catalog.operatorcatalogtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperatorCatalogUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
