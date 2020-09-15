e62a10e601f8837056740886c0d5e48768bd6cb5
Khaled Janania <kjanania@redhat.com>
2020-09-15 17:06:39 -0400

    Fixing tests to match new format.

--- operator-utils-api-service/src/test/java/redhat/ocp4/operators/catalog/utils/cache/OperatorCatalogCacheTests.java
+++ operator-utils-api-service/src/test/java/redhat/ocp4/operators/catalog/utils/cache/OperatorCatalogCacheTests.java
@@ -1,36 +1,22 @@
 package redhat.ocp4.operators.catalog.utils.cache;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.hazelcast.core.HazelcastInstance;
-import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.Resource;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
+import redhat.ocp4.operators.catalog.utils.dto.OperatorDetails;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @TestPropertySource(properties = {
@@ -38,7 +24,7 @@ import java.util.Map;
 })
 public class OperatorCatalogCacheTests {
     private WireMockServer wireMockServer = new WireMockServer(options().port(8090));
-    private static List<Map<String, String>> testOperatorList = new ArrayList<>();
+    private static List<OperatorDetails> testOperatorList = new ArrayList<>();
 
     @Autowired
     OperatorCatalogCache cache;
@@ -47,10 +33,7 @@ public class OperatorCatalogCacheTests {
 
     @BeforeAll
     public static void init() {
-        Map<String, String> testOperatorEntry = new HashMap<>();
-        testOperatorEntry.put("name", "amq-streams");
-        testOperatorEntry.put("version", "v1.0.0");
-        testOperatorList.add(testOperatorEntry);
+        testOperatorList.add(OperatorDetails.builder().operatorName("amq-streams").version("v1.0.0").build());
     }
 
     @Test
