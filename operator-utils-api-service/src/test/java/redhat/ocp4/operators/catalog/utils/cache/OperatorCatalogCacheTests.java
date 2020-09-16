45195a5e5f1c31f431bba255b94b9f0842f7c132
Lev Shulman <lshulman@redhat.com>
2020-09-16 10:05:07 -0600

    removing WireMockServer as it is not needed

--- operator-utils-api-service/src/test/java/redhat/ocp4/operators/catalog/utils/cache/OperatorCatalogCacheTests.java
+++ operator-utils-api-service/src/test/java/redhat/ocp4/operators/catalog/utils/cache/OperatorCatalogCacheTests.java
@@ -1,29 +1,25 @@
 package redhat.ocp4.operators.catalog.utils.cache;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+
 import redhat.ocp4.operators.catalog.utils.dto.OperatorDetails;
-
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @TestPropertySource(properties = {
         "operatorhub.tar.gz.file=classpath:operatorhub-manifests-5-31-20.tar.gz"
 })
 public class OperatorCatalogCacheTests {
-    private WireMockServer wireMockServer = new WireMockServer(options().port(8090));
     private static List<OperatorDetails> testOperatorList = new ArrayList<>();
 
     @Autowired
